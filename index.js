fcb84b116d9df4bc9837f30bdbbefa01569d08d7
ale <ale.g.acosta08@gmail.com>
2019-06-09 20:23:25 -0300

    add root message

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ app.get('/texts', (req, res, next) => {
     })
 });
 
+app.get('/', (res) => {
+  res.send("hello")
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
